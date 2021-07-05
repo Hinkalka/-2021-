1489d3de49a5a643d31c22e89ef39f044640f23d
daniilsergeeg@gmail.com <daniilsergeeg@gmail.com>
2021-07-05 08:52:45 +0300

    Добавьте файлы проекта.

---
+++ Экзамен 2021 ПП/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Экзамен_2021_ПП
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
